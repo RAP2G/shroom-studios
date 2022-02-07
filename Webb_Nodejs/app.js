36e91bf9a95acf2e98427f97d8bb258b724b060c
Soma Szabó <szabosonka@gmail.com>
2022-02-07 19:04:04 +0100

    made routing for frontend

--- Webb_Nodejs/app.js
+++ Webb_Nodejs/app.js
@@ -3,12 +3,18 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 //const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/css', express.static(__dirname + '/views/css'));
+app.use('/style.css', express.static(__dirname + '/views/style.css'));
+app.use('/js', express.static(__dirname + '/views/js'));
+app.use('/img', express.static(__dirname + '/views/img'));
+app.use('/fonts', express.static(__dirname + '/views/fonts'));
 
 // app.use('/admin', adminRoutes);
 app.use(express.static('public'));
